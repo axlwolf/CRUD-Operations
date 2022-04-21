830226c1c76ff6b88ea39b60f4e302a40a83ed50
Axel Lanuza <axell@zoomcatalog.com>
2022-04-20 21:54:35 -0500

    4. Add: Basic logic for create data in js

---
+++ dist/main.dev.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var CrudOperations = function (window, document) {
+  var $form = document.getElementById("form");
+  var $input = document.getElementById("input");
+  var $msg = document.getElementById("msg");
+  var $posts = document.getElementById("posts");
+  var $submitDataBtn = document.getElementById("submitData");
+  var data = {};
+
+  var init = function init() {
+    console.log("Init");
+    bindEvents();
+  };
+
+  var bindEvents = function bindEvents() {
+    $form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      submitData(e);
+    });
+  };
+
+  var submitData = function submitData(e) {
+    console.log(e);
+    console.log("button clicked");
+    formValidation();
+  };
+
+  var formValidation = function formValidation() {
+    console.info("Validation");
+
+    if ($input.value == "") {
+      $msg.innerHTML = "Post cannot be blank";
+      console.warn("failure");
+    } else {
+      console.log("success");
+      $msg.innerHTML = "";
+      acceptData();
+    }
+  };
+
+  var acceptData = function acceptData() {
+    data.text = $input.value;
+    console.table(data);
+    createPost();
+  };
+
+  var createPost = function createPost() {
+    $posts.innerHTML += "\n            <div>\n                <p>".concat(data.text, "</p>\n                <span class=\"options\">\n                    <i onClick=\"editPost(this)\" class=\"fas fa-edit\"></i>\n                    <i onClick=\"deletePost(this)\" class=\"fas fa-trash-alt\"></i>\n                </span>\n            </div>\n        ");
+    $input.value = "";
+  };
+
+  return {
+    Init: init
+  };
+}(window, document);
+
+if (document.readyState === "complete" || document.readyState !== "loading" && !document.documentElement.doScroll) {
+  CrudOperations.Init();
+} else {
+  document.addEventListener("DOMContentLoaded", CrudOperations.Init);
+}
---
+++ main.js
@@ -0,0 +1,73 @@
+const CrudOperations = ((window, document) =>  {
+    const $form = document.getElementById("form");
+    const $input = document.getElementById("input");
+    const $msg = document.getElementById("msg");
+    const $posts = document.getElementById("posts");   
+    const $submitDataBtn = document.getElementById("submitData");
+    let data = {};    
+
+    let init = () => { 
+        console.log("Init");
+        bindEvents();
+    }
+
+    let bindEvents = () => {
+        $form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            submitData(e);
+        });
+    }
+
+    let submitData = (e) => {
+        console.log(e);
+        console.log("button clicked");
+
+        formValidation();
+    }
+
+    let formValidation = () => {
+        console.info("Validation");
+        if ($input.value == "") {
+            $msg.innerHTML = "Post cannot be blank";
+            console.warn("failure");
+        } else {
+            console.log("success");
+            $msg.innerHTML = "";
+            acceptData();
+        }
+    }
+
+    let acceptData = () => {
+        data.text = $input.value;
+        console.table(data);
+        createPost();
+    }
+
+    let createPost = () => {
+        $posts.innerHTML += `
+            <div>
+                <p>${data.text}</p>
+                <span class="options">
+                    <i onClick="editPost(this)" class="fas fa-edit"></i>
+                    <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+                </span>
+            </div>
+        `;
+        $input.value = "";
+    }
+
+    return {
+        Init: init
+    }
+})(window, document);
+
+
+if (
+    document.readyState === "complete" ||
+    (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+    CrudOperations.Init();
+} else {
+    document.addEventListener("DOMContentLoaded", CrudOperations.Init);
+}
+
